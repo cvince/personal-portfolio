6ca2eef471269b0464856dc6755b080f660f372b
Vincent Chan <vincent@vincentcchan.com>
2014-03-05 13:51:49 -0800

    added ga tracking

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 
@@ -10,3 +10,13 @@
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <link href='<?php echo ROOT.'css/global.css'?>' rel='stylesheet' type='text/css'>
 </head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-48713503-1', 'vincentcchan.com');
+  ga('send', 'pageview');
+
+</script>
--- js/func.js
+++ js/func.js
@@ -10,41 +10,43 @@ $.prototype.wait = function( ms, callback ) {
 };
 
 $('a.case-link').click(function(event){
-	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;  
-	
+	(event.preventDefault) ? event.preventDefault() : event.returnValue = false;
+
     var thisHref = $(this).attr('href');
     console.log();
 
-    $('#case-study-article').removeClass('hidden closed').wait(5,function(){$(this).addClass('slideLeft scrollable');}).wait(500, function(){$('body').addClass('noScroll'); } );
+    $('#case-study-article').removeClass('hidden closed')
+    .wait(5,function(){$(this).addClass('slideLeft scrollable');})
+    .wait(500, function(){$('body').addClass('noScroll'); } );
 
     var location = $(this).attr('href')+'/case.html';
 
     $.post(location, function(data){
-        $('#case-content').html(data);  
+        $('#case-content').html(data);
         window.history.pushState(null, null, thisHref);
     });
     $('.close-case').css('opacity','1');
-    $('article').scrollTop(0);             
+    $('article').scrollTop(0);
 
 
 
 })
 
 $('.close-case').click(function(event){
-    (event.preventDefault) ? event.preventDefault() : event.returnValue = false;  
+    (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
         window.history.pushState(null, null, '/');
     $('body').removeClass('noScroll');
     $(this).css('opacity','0');
 	$('#case-study-article').removeClass('slideLeft scrollable').addClass('ani').wait(50,function(){$(this).addClass('closed')}).wait(500, function() {
-	    $(this).addClass('hidden').removeClass('ani');    
+	    $(this).addClass('hidden').removeClass('ani');
 	});
     return false;
 });
 
 $('nav .scrollTo').click(function(){
     (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
-    var thisHref = $(this).attr('href');    
-    window.history.pushState(null, null, thisHref);    
+    var thisHref = $(this).attr('href');
+    window.history.pushState(null, null, thisHref);
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'data-href') ).offset().top
     }, 500);
